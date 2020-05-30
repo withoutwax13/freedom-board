2375e5ea36dd697facb696e969e55aac2f19b91b
JP Valera <johnpatrickyusoresvalera@gmail.com>
2020-05-30 18:04:29 +0800

    Added redux-persist for data persistence

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import 'semantic-ui-css/semantic.min.css'
+
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
 
 import { App } from './components/App.js'
 import reducers from './reducers'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+
+let store = createStore(persistedReducer)
+let persistor = persistStore(store)
+
+
 ReactDOM.render(
-	<Provider store={createStore(reducers)}>
-		<App />
+	<Provider store={store}>
+		<PersistGate loading={null} persistor={persistor}>
+			<App/>
+		</PersistGate>
 	</Provider>
 	,
 	document.querySelector('#root')
